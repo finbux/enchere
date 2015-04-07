ef91d71209d03384b4df3c1944ce6a0c6e136511
LASSON Jérémy <finbux@gmail.com>
2015-04-07 20:17:54 +0200

    ajout des rubalise sur git + maj responsive design

---
+++ src/Enchere/Bundle/MainBundle/Entity/Conteneur.php
@@ -0,0 +1,184 @@
+<?php
+
+namespace Enchere\Bundle\MainBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Conteneur
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Enchere\Bundle\MainBundle\Entity\ConteneurRepository")
+ */
+class Conteneur
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="prix", type="float")
+     */
+    private $prix;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text")
+     */
+    private $description;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="image", type="text")
+     */
+    private $image;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="lastTime", type="float")
+     */
+    private $lastTime;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Conteneur
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set prix
+     *
+     * @param float $prix
+     * @return Conteneur
+     */
+    public function setPrix($prix)
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    /**
+     * Get prix
+     *
+     * @return float 
+     */
+    public function getPrix()
+    {
+        return $this->prix;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return Conteneur
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set image
+     *
+     * @param string $image
+     * @return Conteneur
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    /**
+     * Get image
+     *
+     * @return string 
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * Set lastTime
+     *
+     * @param float $lastTime
+     * @return Conteneur
+     */
+    public function setLastTime($lastTime)
+    {
+        $this->lastTime = $lastTime;
+
+        return $this;
+    }
+
+    /**
+     * Get lastTime
+     *
+     * @return float 
+     */
+    public function getLastTime()
+    {
+        return $this->lastTime;
+    }
+}
